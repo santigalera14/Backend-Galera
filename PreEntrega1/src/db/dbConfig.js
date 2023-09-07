f9d88cb521389c723df54a4941c20a96379aa895
Santiago <107081689+santigalera14@users.noreply.github.com>
2023-09-07 20:17:08 -0300

    Add files via upload

--- PreEntrega1/src/db/dbConfig.js
+++ PreEntrega1/src/db/dbConfig.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const URI = 'mongodb+srv://santiagogalera14:piaysan0610@cluster0.dtluphu.mongodb.net/?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://Codersantiagogalera14:piaysan0610@cluster0.lntpo2j.mongodb.net/291800SW?retryWrites=true&w=majority';
 mongoose.connect(URI)
     .then(() => console.log('Conectado a la base de datos'))
     .catch(err => console.log(err));
